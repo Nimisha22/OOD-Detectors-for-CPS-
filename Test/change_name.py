727b6d1b9f5e00ccfbe8d67275adb8c44ad7d9b4
Nimisha Doshi <nimisha12doshi@gmail.com>
2022-09-26 11:18:41 +0800

    Code to rename the photos present in folder

---
+++ Test/change_name.py
@@ -0,0 +1,24 @@
+# Code to rename the photos present in folder
+import os
+
+folder = r'/home/n2202864a/Downloads/test1/No_Fog/'
+count = 1
+
+# count increase by 1 in each iteration
+# iterate all files from a directory
+for file_name in os.listdir(folder):
+    # Construct old file name
+    source = folder + file_name
+
+    # Adding the count to the new file name and extension
+    destination = folder + "N_" + str(count) + ".png"
+
+    # Renaming the file
+    os.rename(source, destination)
+    count += 1
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+res = os.listdir(folder)
+print(res)
